f7a7e33c6c67785856a67e7f4caeb3383528269c
Daniel Wood <daniel.a.wood.jr@gmail.com>
2015-07-10 14:51:14 -0600

    added additional functionality

--- test/testing.js
+++ test/testing.js
@@ -3,43 +3,54 @@ var vargs = require("../lib/argparser.js");
 
 
 describe("Tags", function(){
-    describe("#parse()", function(){
-        it("should parse args to returned object", function(){
+    describe("#parseArgs()", function(){
+        it("should parse args to returned object", function(done){
             var args = ["-M=65","--major=4", "-m=2", "-p=1"];
-            console.log("args:: "+ args);
-            var results = vargs.parse(args, function(results){
+	    setTimeout(function(){
+		
+	    
+            var results = vargs.parseArgs(args, function(results){
 
+		expect(results).to.have.a.property("major", 4);
+		expect(results).to.have.a.property("minor", 2);
+		expect(results).to.have.a.property("patch", 1);
 	    });
-            
-            expect(results).to.have.a.property("major", 4);
-            expect(results).to.have.a.property("minor", 2);
-            expect(results).to.have.a.property("patch", 1);
-            
+            done();
+	    }, 200);
             
         });
     });
     describe("#loadFile(filename)", function(){
 	it("should load the filename and return the obj representation", function(done){
 	    setTimeout(function(){
-		vargs.loadFile("package.json", function(err, result){
+	    vargs.loadFile("package.json", function(result){
+		console.log("result === " + result);
 		    expect(result).to.exist;
     		});
-		done();
+	    done();
 	    }, 200);
 	}); 
     });
-//  describe("#update(data)", function(){
-//      it("should update update the version number and return the object", function(done)
-//	 {
-//	     setTimeout(function(){
-//		 vargs.loadFile("package.json", function(err, result){
-//		     vargs.saveFile(result, "package.json", function(err, complete){
-//			 expect(complete).to.equal("true");
-//		     });
-//		 });
-//	     }, 200);
-//	});
-  //  });
+
+    describe("#saveFile(data, filename, done)", function(){
+	it("should save the file and return true if successful", function(done){
+	    var data = vargs.loadFile("package.json", function(result){
+		setTimeout(function(){
+		    vargs.saveFile(data, "package.json", function(result){
+			expect(result).to.be.true;
+		    });
+		},200);
+	    });
+		    done();
+	    
+	});
+    });
+
+    describe("#bumpVersion(data, done)", function(){
+	it("should bump the package version by values passed in", function(done){
+	    var data = vargs.loadFile("package.json", function(err
+	});
+    });
 
 
 });
