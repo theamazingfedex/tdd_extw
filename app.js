3893cdb7fe566b63352a5c96a268ed3d4daf2222
Daniel Wood <daniel.a.wood.jr@gmail.com>
2015-07-13 00:18:56 -0600

    I think it's done now...

--- app.js
+++ app.js
@@ -13,23 +13,24 @@ use the following parameters to update the version following semver guidelines a
 ***/
 var version = "0.0.0";
 var filename = "package.json";
-
+var vargs = "none";
 //Initializes the application
 
 console.log("initializing");
 (0, _libArgparserJs.parseArgs)(process.argv, function (results) {
-	version = results;
+	vargs = results;
 });
 (0, _libArgparserJs.loadFile)(filename, function (results) {
-	(0, _libArgparserJs.bumpVersion)(results, version, function (results2) {
-		(0, _libArgparserJs.saveFile)(results2, filename, function (results) {
-			if (!results2) console.log("failed to bump the module version to " + version);else {
+	(0, _libArgparserJs.bumpVersion)(results, vargs, function (results2) {
+		version = results2.version;
+		(0, _libArgparserJs.saveFile)(results2, filename, function (results3) {
+			if (!results3) console.log("failed to bump the module version to " + version);else {
 				console.log("succeeded in bumping the module version to " + version);
-				(0, _libArgparserJs.commitToLocalGit)(version, function (results3) {
-					if (!results3) console.log("failed to commit to local git repository; Is one set-up?");else {
-						(0, _libArgparserJs.addGitTag)(version, "ESBump added git Tag v" + version, function (results4) {
-							(0, _libArgparserJs.pushToRemote)(null, function (results) {
-								console.log("completed pushing to remote:\n" + results);
+				(0, _libArgparserJs.commitToLocalGit)(version, function (results4) {
+					if (!results4) console.log("failed to commit to local git repository; Is one set-up?");else {
+						(0, _libArgparserJs.addGitTag)(version, "ESBump added git Tag v" + version, function (results5) {
+							(0, _libArgparserJs.pushToRemote)(null, vargs.username, vargs.password, function (results6) {
+								console.log("completed pushing to remote:\n" + results6);
 							});
 						});
 					}
