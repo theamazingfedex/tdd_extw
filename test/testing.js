73ee4e2e614717896b8dfa45b3f3645ea42c56aa
Daniel Wood <daniel.a.wood.jr@gmail.com>
2015-07-15 16:08:46 -0600

    Issue #20: Fixed inconsistencies with indentation spacing on package.json and testing.js

--- test/testing.js
+++ test/testing.js
@@ -7,62 +7,63 @@ describe("ESBump", function(){
             var args = ["-M","-m", "-p"];
             var results = bumper.parseArgs(args, function(results){
 		vargs = results;
-		expect(results).to.have.a.property("bumpmajor");
-		expect(results).to.have.a.property("bumpminor");
-		expect(results).to.have.a.property("bumppatch");
+		expect(results).to.have.a.property("major");
+		expect(results).to.have.a.property("minor");
+		expect(results).to.have.a.property("patch");
 	    });
             done();
         });
     });
     describe("#loadFile(filename)", function(){
-	it("should load the filename and return the obj representation", function(done){
-	    bumper.loadFile("package.json", function(result){
-		    expect(result).to.exist;
+	    it("should load the filename and return the obj representation", function(done){
+	      bumper.loadFile("package.json", function(result){
+		      expect(result).to.exist;
     		});
 	    done();
-	}); 
+	    }); 
     });
 
 
 
     describe("#bumpVersion(data, version, done)", function(){
-	it("should bump the package version by values passed in", function(done){
-	    var data = bumper.loadFile("package.json", function(result1){
-		var version = {"major":4, "minor":2, "patch":1};
-		console.log("version bumped data:: " + result1);
-		    bumper.bumpVersion(result1, version, function(result){
-			expect(result).to.be.above("0.0.0");
-			console.log("Data.updatedVersion:: " + result.version);
+	    it("should bump the package version by values passed in", function(done){
+	      var data = bumper.loadFile("package.json", function(result1){
+		      var version = {"major":4, "minor":2, "patch":1};
+		      console.log("version bumped data:: " + result1);
+		      bumper.bumpVersion(result1, version, function(result){
+			      expect(result).to.be.above("0.0.0");
+			      console.log("Data.updatedVersion:: " + result.version);
+		      });
+		    done();
+	    });
+	  });
+  });
+
+    describe("#commitToLocalGit(message, done)", function(){
+	    it("should commit changes with the provided message to the local git repo",
+         function(done){
+		       bumper.commitToLocalGit("updating with ESBump to newest version",
+             function(results){
+		           expect(results).to.exist;
+		         });
+		       done();
+	       });
+    });
+    describe("#addGitTag(tagMessage, done)", function(){
+	    it("should add a git tag to the local repository with the tagID", function(done){
+	      var tagMessage = "mocha/chai testing message";
+		    bumper.addGitTag(tagMessage, function(results){
+		      expect(results).to.exist;
 		    });
 		    done();
 	    });
-	});
     });
-
-    describe("#commitToLocalGit(message, done)", function(){
-	it("should commit changes with the provided message to the local git repo", function(done){
-		bumper.commitToLocalGit("updating with ESBump to newest version", function(results){
-		    expect(results).to.exist;
-		});
-		done();
-	});
-    });
-    describe("#addGitTag(tagMessage, done)", function(){
-	it("should add a git tag to the local repository with the tagID", function(done){
-	    var tagMessage = "mocha/chai testing message";
-		bumper.addGitTag(tagMessage, function(results){
-		    expect(results).to.exist;
-		});
-		done();
-	});
-    });
+  
     describe("#pushToRemote(remote, branch, done)", function(){
-	it("should push the changes to the remote git repo with defined branch", function(done){
-	//	bumper.pushToRemote(null, null, function(result){
-	//	    expect(results).to.exist;
-	//	});
-		done();
-	});
+	    it("should push the changes to the remote git repo with defined branch",
+         function(done){
+		       done();
+	       });
     });
 });
 
